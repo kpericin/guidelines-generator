b8b05423bb583a8443c7a02318fb25f9e194ac15
Katja Pericin <4922095+kpericin@users.noreply.github.com>
2025-12-29 18:23:17 +0100

    chore: publish to GitHub pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.GITHUB_PAGES === 'true' ? '/guidelines-generator/' : '/',
+  build: {
+    outDir: process.env.GITHUB_PAGES === 'true' ? 'docs' : 'dist',
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
-})
+});
